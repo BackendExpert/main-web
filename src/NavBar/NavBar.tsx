c289cac83473991c6cd8ebab536185d58953c593
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-16 12:01:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -14,20 +14,20 @@ const NavBar = () => {
             </div>
             <div className="">
                 <div onClick={() => SetOpenMenu(!menuOpen)} className="text-3xl absolute right-7 top-4 cursor-pointer md:hidden">
-                    <IonIcon name="menu"></IonIcon>
+                    <IonIcon name={menuOpen ? 'menu' : 'close'}></IonIcon>
                 </div>
 
                 <ul className={`md:flex md:items-center absolute md:static md:pb-0 pb-10 md:bg-gray-700 bg-gray-800 md:z-auto z-[-1] md:mt-0 mt-[-15px] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${menuOpen ? 'top-20' : 'top-[-500px]'}`}>
                     <li className="px-4 md:py-0 py-5">Home</li>
-                    <li className="px-4 md:py-0 py-5">
-                        <div className="w-auto">
+                    <li className="px-4 mr-4 md:py-0 py-5">
+                        <div className="w-auto flex">
                             <div className="">About</div>
-                            <div onClick={() => SetOpenMenu(!menuOpen)} className="text-3xl absolute pl-8 cursor-pointer">
-                                <IonIcon name="caret-down"></IonIcon>
+                            <div onClick={() => SetOpenMenu(!menuOpen)} className="text-md absolute ml-12 pt-1 cursor-pointer">
+                                <IonIcon name="chevron-down"></IonIcon>
                             </div>
                         </div>
 
-                        <ul className="md:hidden">
+                        <ul className="hidden">
                             <li>hi all</li>
                         </ul>
                     </li>
