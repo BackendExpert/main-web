dbd592752fe90c5c4c28de47d7325e7886330dee
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-16 11:31:41 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -3,17 +3,18 @@ import IonIcon from "@reacticons/ionicons"
 const NavBar = () => {
   return (
     <div className="text-white bg-gray-700 w-full h-16">
-        <div className="">
-            <div className="flex py-4 px-8">
+        <div className="justify-between flex py-4 px-8">
+            <div className="flex ">
                 <span className=""><IonIcon size="large" name="newspaper"></IonIcon></span>
                 <span className="pl-4 pt-0 text-xl">Logo</span>
             </div>
+            <div className="">
+                <ul>
+                    <li>Home</li>
+                </ul>
+            </div>
         </div>
-        <div className="">
-            <ul>
-                <li>Home</li>
-            </ul>
-        </div>
+
     </div>
   )
 }
