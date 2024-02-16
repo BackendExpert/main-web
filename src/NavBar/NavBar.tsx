19923118ee0176e5d653f5d245e42baa25fbad13
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-16 11:52:12 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 const NavBar = () => {
     const [menuOpen, SetOpenMenu] = useState(false)
 
+
   return (
     <div className="text-white bg-gray-700 w-full h-16">
         <div className="justify-between flex py-4 px-8">
@@ -17,13 +18,13 @@ const NavBar = () => {
                 </div>
 
                 <ul className={`md:flex md:items-center absolute md:static md:pb-0 pb-10 md:bg-gray-700 bg-gray-800 md:z-auto z-[-1] md:mt-0 mt-[-15px] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${menuOpen ? 'top-20' : 'top-[-500px]'}`}>
-                    <li className="px-4">Home</li>
-                    <li className="px-4">About</li>
-                    <li className="px-4">News & Events</li>
-                    <li className="px-4">Academic</li>
-                    <li className="px-4">Publication</li>
-                    <li className="px-4">Careers</li>
-                    <li className="px-4">Contact</li>
+                    <li className="px-4 md:py-0 py-5">Home</li>
+                    <li className="px-4 md:py-0 py-5">About</li>
+                    <li className="px-4 md:py-0 py-5">News & Events</li>
+                    <li className="px-4 md:py-0 py-5">Academic</li>
+                    <li className="px-4 md:py-0 py-5">Publication</li>
+                    <li className="px-4 md:py-0 py-5">Careers</li>
+                    <li className="px-4 md:py-0 py-5">Contact</li>
                 </ul>
             </div>
         </div>
