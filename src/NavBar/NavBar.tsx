8baa57297c6d5eed5cd717017c6d12e901872124
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-16 12:08:49 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/NavBar/NavBar.tsx
+++ src/NavBar/NavBar.tsx
@@ -19,13 +19,19 @@ const NavBar = () => {
 
                 <ul className={`md:flex md:items-center absolute md:static md:pb-0 pb-10 md:bg-gray-700 bg-gray-800 md:z-auto z-[-1] md:mt-0 mt-[-15px] left-0 w-full md:w-auto md:pl-0 pl-9 transition-all duration-500 ease-in ${menuOpen ? 'top-20' : 'top-[-500px]'}`}>
                     <li className="px-4 md:py-0 py-5">Home</li>
-                    <li className="px-4 mr-4 md:py-0 py-5">
-                        <div className="w-auto flex">
-                            <div className="">About</div>
-                            <div onClick={() => SetAboutMenu(!aboutOpen)} className="text-md absolute ml-12 pt-1 cursor-pointer">
-                                <IonIcon name="chevron-down"></IonIcon>
+                    <li className="px-4 mr-12 md:py-0 py-5">
+
+                            {/* <div className="">About</div> */}
+                            <div onClick={() => SetAboutMenu(!aboutOpen)} className="text-md absolute ml-0 pt-1 cursor-pointer">
+                                <div className="flex -mt-[15px]">
+                                    <div className="">About</div>
+                                    <div className="pt-1 pl-1">
+                                        <IonIcon name={aboutOpen ? 'chevron-down' : 'chevron-up'}></IonIcon>
+                                    </div>
+                                </div>
+
                             </div>
-                        </div>
+
 
                         <ul className="hidden">
                             <li>hi all</li>
