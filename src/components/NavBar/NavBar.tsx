6d65ed44c0aa3611958cd99f618f02fba016de24
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-16 16:41:49 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -73,7 +73,7 @@ const NavBar = () => {
                                             <p className="md:hidden pl-2 mt-1"><IonIcon name={openins ? 'chevron-up' : 'chevron-down'}></IonIcon></p>
                                         </div>
                                     </div>
-                                    <div className={`pl-3 pt-2 -pb-4 ${openins ? 'static' : 'md:visible hidden'}`}>
+                                    <div className={`pl-3 pt-2 -pb-4 ${openins ? 'static' : 'md:inline-block hidden'}`}>
                                         {/* fetch data from institute */}
                                         {
                                             institue.map((ins) => (
