eedc1df27b778a49b9838654816b68788089a528
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-16 11:24:06 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import NavBar from "./NavBar/NavBar";
+
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <NavBar />
   )
 }
