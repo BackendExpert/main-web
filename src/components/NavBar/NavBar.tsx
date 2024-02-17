35a78da95155fb0e2833e22f58109f18074caaee
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-17 20:33:57 +0530

    UPdate
    Co-Authored-By: JehanKandy <jehankandy@gmail.com>

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -387,17 +387,17 @@ const NavBar = () => {
                     </li>
                     <li className="px-4 mr-20 md:py-0 pt-6 pb-6">
                         {/* Open Careers submenu */}
-                        <div onClick={() => SetPubOpen(!oepnPub)} className="text-md absolute ml-0 pt-1 cursor-pointer mr-12">
+                        <div onClick={() => SetCareersOpen(!openCareers)} className="text-md absolute ml-0 pt-1 cursor-pointer mr-12">
                             <div className="flex -mt-[16px]">
                                 <div className="">Careers</div>
                                 <div className="pt-1 pl-1 ">
-                                    <IonIcon name={oepnPub ? 'chevron-up' : 'chevron-down'}></IonIcon>
+                                    <IonIcon name={openCareers ? 'chevron-up' : 'chevron-down'}></IonIcon>
                                 </div>
                             </div>
                          </div>
 
                         {/* Open Careers submenu Start */}
-                         <div className={`md:pr-8 pt-6 -ml-5 md:pl-8 md:pt-8 md:pb-8 rounded md:ml-[4%] md:w-11/12 md:mt-10 bg-gray-800 md:items-center md:pb-0 md:absolute md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9  ${oepnPub ? 'static' : 'hidden'}`}>
+                         <div className={`md:pr-8 pt-6 -ml-5 md:pl-8 md:pt-8 md:pb-8 rounded md:ml-[4%] md:w-11/12 md:mt-10 bg-gray-800 md:items-center md:pb-0 md:absolute md:z-auto z-[-1] left-0 w-full md:w-auto md:pl-0 pl-9  ${openCareers ? 'static' : 'hidden'}`}>
                             <div className="md:grid grid-cols-3 gap-2">
                                 {
                                     publications.map((publication) =>(
