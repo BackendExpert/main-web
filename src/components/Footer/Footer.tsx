fbf8f54f8bb432a8566c83b9de659f1eba4871a0
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-18 19:41:36 +0530

    Udpaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,9 @@ const Footer = () => {
   return (
     <div className="">
         <div className="contaier mx-auto px-12">
-            Footer
+            <div className="md:grid grid-cols-4 gap-2">
+                <div className=""></div>
+            </div>
         </div>
     </div>
   )
