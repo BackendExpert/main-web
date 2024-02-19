a5bf1e5838d214c5f470e2176f0ac7d19a301712
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 15:08:47 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Scientist/Scientist.tsx
+++ src/components/Scientist/Scientist.tsx
@@ -15,7 +15,7 @@ const Scientist = () => {
     return (
     <div className="container mx-auto lg:px-32 px-8 py-20">
         <h1 className="text-center font-semibold text-3xl">Scientists</h1>
-        <div className="">
+        <div className="my-5">
             <div className="lg:grid grid-cols-5 gap-4">
                 <div className="">
                     <p className="">Sort By</p>
@@ -39,6 +39,28 @@ const Scientist = () => {
                                 ))
                             }
                         </select>
+                    </div>
+                </div>
+                <div className="">
+                    <button className="mt-5 bg-[#293352] py-2 px-8 text-white rounded-3xl duration-500 hover:shadow-xl">
+                        Apply
+                    </button>
+                </div>
+            </div>
+            <div className="my-4">
+                <div className="lg:grid grid-cols-3 gap-4">
+                    <div className="mb-12">
+                        <img src="https://wallpaperaccess.com/full/1617671.jpg" alt="" className="rounded"/>
+                        <div className="-mt-16 text-white pl-4">
+                            <p className="font-bold">Prof.Ananda Kulasooriya</p>
+                            <p className="">Honorary Visiting Research Professer</p>
+                        </div>
+                    </div>
+                    <div className="">
+                        <img src="https://wallpaperaccess.com/full/1617671.jpg" alt="" className="rounded"/>
+                    </div>
+                    <div className="">
+                        <img src="https://wallpaperaccess.com/full/1617671.jpg" alt="" className="rounded"/>
                     </div>
                 </div>
             </div>
