91f527440ed343b2a87c5ed978cb3219371c40d1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 14:50:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Scientist/Scientist.tsx
+++ src/components/Scientist/Scientist.tsx
@@ -16,14 +16,26 @@ const Scientist = () => {
     <div className="container mx-auto lg:px-32 px-8 py-20">
         <h1 className="text-center font-semibold text-3xl">Scientists</h1>
         <div className="">
-            <div className="lg:grid grid-cols-5 gap-2">
+            <div className="lg:grid grid-cols-5 gap-4">
                 <div className="">
                     <p className="">Sort By</p>
                     <div className="">
-                        <select name="" id="" className="w-full border rounded py-2 px-2">
+                        <select name="" id="" className="w-full border rounded py-2">
                             {
                                 sortBy.map((sort) => (
                                     <option value="" className="">{sort.name}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                </div>
+                <div className="">
+                    <p className="">Sort By</p>
+                    <div className="">
+                        <select name="" id="" className="w-full border rounded py-2">
+                            {
+                                orderBy.map((order) => (
+                                    <option value="" className="">{order.name}</option>
                                 ))
                             }
                         </select>
