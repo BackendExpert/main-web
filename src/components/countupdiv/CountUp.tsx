bd7c2b57b8037aa484e4ec64d4b6a7a5e2b55631
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 09:33:27 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/countupdiv/CountUp.tsx
+++ src/components/countupdiv/CountUp.tsx
@@ -1,10 +1,16 @@
+//import reacttions 
 import IonIcon from "@reacticons/ionicons"
+
+//import up package
+import UpCount from "react-countup"
 
 const CountUp = () => {
   return (
     <div className="container mx-auto px-32">
-        <div className="md:grid grid-cols-5 gap-2">
+        <div className="md:grid grid-cols-5 gap-2 md:my-12">
             <div className="">
+                <span className="text-3xl"><IonIcon name="newspaper" size="large"></IonIcon></span>
+                <span className="">Patient</span>
                 <span className=""></span>
             </div>
         </div>
