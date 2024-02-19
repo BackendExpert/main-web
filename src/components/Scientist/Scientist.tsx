d10c16d01cb16a7981cdf062189f44acfb5666c1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 12:26:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Scientist/Scientist.tsx
+++ src/components/Scientist/Scientist.tsx
@@ -2,7 +2,7 @@
 const Scientist = () => {
   return (
     <div className="container mx-auto px-32 lg:py-20">
-        <h1 className="text-3xl font-semibold">Scientist</h1>
+        <h1 className="text-3xl font-semibold text-center">Scientist</h1>
     </div>
   )
 }
