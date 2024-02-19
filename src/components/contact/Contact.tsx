07a0ed0f31e0d19d4bc84c5c4b2b2ca060599096
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 11:12:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -63,6 +63,32 @@ const Contact = () => {
                         </div>
                     </div>
                 </div>
+                <div className="">
+                    <p className="text-xl pt-4 pb-4 font-semibold">Colombo Office</p>
+                    <div className="">
+                        <p>National Institute of Fundemental Studies</p>
+                        <p>No.380/71,72, Bauddhaloka Mawatha</p>
+                        <p>Colombo 07</p>
+                        <p>Sri Lanka</p>
+                    </div>
+
+                    <div className="flex py-8">
+                        <p className="py-4">
+                            <IonIcon name="call" size="large"></IonIcon>
+                        </p>
+                        <div className="pl-2 pt-5">
+                            <p>(+94) 11 26 97 538</p>
+                        </div>
+                    </div>
+                    <div className="flex pt-2 pb-4">
+                        <p className="py-2">
+                            <IonIcon name="mail" size="large"></IonIcon>
+                        </p>
+                        <div className="pl-2 pt-3">
+                            <p>info@nifs.ac.lk</p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
