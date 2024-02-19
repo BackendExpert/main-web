261c99288673c0e7236ba5619b51412fbd6eaf46
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 09:36:44 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/countupdiv/CountUp.tsx
+++ src/components/countupdiv/CountUp.tsx
@@ -9,10 +9,10 @@ const CountUp = () => {
     <div className="container mx-auto px-32">
         <div className="md:grid grid-cols-5 gap-2 md:my-12">
             <div className="border">
-                <div className="">
+                <div className="text-center">
                     <p className="text-3xl"><IonIcon name="newspaper" size="large"></IonIcon></p>
-                    <p className="">Patient</p>
-                    <p className=""><UpCount end={100} /></p>
+                    <p className="text-2xl">Patient</p>
+                    <p className="text-2xl"><UpCount end={100} /></p>
                 </div>
             </div>
         </div>
