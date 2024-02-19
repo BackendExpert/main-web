0a2db77cf005df4db4d9403aa5e9c199ae1a5de6
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 09:46:28 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/countupdiv/CountUp.tsx
+++ src/components/countupdiv/CountUp.tsx
@@ -6,26 +6,30 @@ import UpCount from "react-countup"
 
 
 let coutupInfor = [
-    {name: "Patient", logo: <IonIcon name="newspaper" size="large"></IonIcon>, coutdata : 100}
+    {name: "Patient", logo: <IonIcon name="newspaper" size="large"></IonIcon>, coutdata : 100},
+    {name: "Patient", logo: <IonIcon name="newspaper" size="large"></IonIcon>, coutdata : 100},
+    {name: "Patient", logo: <IonIcon name="newspaper" size="large"></IonIcon>, coutdata : 100},
+    {name: "Patient", logo: <IonIcon name="newspaper" size="large"></IonIcon>, coutdata : 100},
+    {name: "Patient", logo: <IonIcon name="newspaper" size="large"></IonIcon>, coutdata : 100},    
 ];
 
 const CountUp = () => {
   return (
     <div className="container mx-auto px-32">
         <div className="md:grid grid-cols-5 gap-2 md:my-12">
-            <div className="border py-8">
-                <div className="text-center">
-                    {
-                        coutupInfor.map((countinfo) => (
+           {
+                coutupInfor.map((countinfo) => (
+                    <div className="border py-8">
+                        <div className="text-center">
                             <div className="">
                                 <p className="text-3xl">{countinfo.logo}</p>
                                 <p className="text-2xl">{countinfo.name}</p>
-                                <p className="text-2xl"><UpCount end={countinfo.coutdata} /></p>
+                                <p className="text-2xl font-semibold"><UpCount end={countinfo.coutdata} /></p>
                             </div>
-                        ))
-                    }
-                </div>
-            </div>
+                        </div>
+                    </div>
+                ))
+            }
         </div>
     </div>
   )
