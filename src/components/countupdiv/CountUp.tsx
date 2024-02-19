aaaba3f50cb7b7fda524a4d98400c123273fde4b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 09:22:55 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/countupdiv/CountUp.tsx
+++ src/components/countupdiv/CountUp.tsx
@@ -1,7 +1,11 @@
 const CountUp = () => {
   return (
     <div className="container mx-auto px-32">
-        
+        <div className="md:grid grid-cols-5 gap-2">
+            <div className="">
+                
+            </div>
+        </div>
     </div>
   )
 }
