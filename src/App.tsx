1c23cb9fe2e6588939449a4d51994cb28d74c3cf
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 09:16:16 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
+import CountUp from "./components/countupdiv/CountUp";
 
 export default function App() {
   return (
     <div className="">
       <NavBar />
-
+        <CountUp />
       <Footer />
     </div>
   )
