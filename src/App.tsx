0fc8189fa0d7332efaf6c75cf54e0fa88e026464
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 10:45:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
-import CountUp from "./components/countupdiv/CountUp";
+// import CountUp from "./components/countupdiv/CountUp";
 import Contact from "./components/contact/Contact";
 
 export default function App() {
