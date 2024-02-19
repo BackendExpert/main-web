f94ebbc9196796191193394ab9dc4dcf03c48976
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 09:43:45 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/countupdiv/CountUp.tsx
+++ src/components/countupdiv/CountUp.tsx
@@ -13,14 +13,14 @@ const CountUp = () => {
   return (
     <div className="container mx-auto px-32">
         <div className="md:grid grid-cols-5 gap-2 md:my-12">
-            <div className="border">
+            <div className="border py-8">
                 <div className="text-center">
                     {
                         coutupInfor.map((countinfo) => (
                             <div className="">
                                 <p className="text-3xl">{countinfo.logo}</p>
                                 <p className="text-2xl">{countinfo.name}</p>
-                                <p className="text-2xl">{countinfo.coutdata}</p>
+                                <p className="text-2xl"><UpCount end={countinfo.coutdata} /></p>
                             </div>
                         ))
                     }
