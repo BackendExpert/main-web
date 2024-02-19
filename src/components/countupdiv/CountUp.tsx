6e394b7e78d90be24db00d034506d9ee4244859f
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 09:24:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/countupdiv/CountUp.tsx
+++ src/components/countupdiv/CountUp.tsx
@@ -1,9 +1,11 @@
+import IonIcon from "@reacticons/ionicons"
+
 const CountUp = () => {
   return (
     <div className="container mx-auto px-32">
         <div className="md:grid grid-cols-5 gap-2">
             <div className="">
-                
+                <span className=""></span>
             </div>
         </div>
     </div>
