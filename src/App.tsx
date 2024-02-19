2f88c042a44b0a9d78fdce8ea0c93ba6256ace1c
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 12:59:47 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import Scientist from "./components/Scientist/Scientist";
 
 export default function App() {
   return (
-    <div className="">
+    <div>
       <NavBar />
         {/* <CountUp /> */}
 
--- src/components/Scientist/Scientist.tsx
+++ src/components/Scientist/Scientist.tsx
@@ -1,7 +1,16 @@
 
 const Scientist = () => {
   return (
-    <div>Scientist</div>
+    <div className="container mx-auto lg:px-32 px-8 py-20">
+        <h1 className="text-center font-semibold text-3xl">Scientists</h1>
+        <div className="">
+            <div className="lg:grid grid-cols-5 gap-2">
+                <div className="">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
