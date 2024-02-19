700b62261987dd11e34974130dc417863c6d7334
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 12:34:31 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Scientist/Scientist.tsx
+++ src/components/Scientist/Scientist.tsx
@@ -9,7 +9,7 @@ const Scientist = () => {
             <div className="lg:grid grid-cols-5 gap-4">
                 <div className="">
                     <p className="font-semibold">Sort by</p>
-                    <div className="border my-2 py-2 px-2 rounded flex">
+                    <div className="border my-2 py-2 px-2 rounded flex justify-between">
                         <p className="">First Name</p>
                         <p className="text-xl pt-0"><IonIcon name="caret-down"></IonIcon></p>
                     </div>
