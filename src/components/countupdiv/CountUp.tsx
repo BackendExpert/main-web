039c1947e1b1d14a60ff799ecedb68b143996a59
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 09:39:20 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/countupdiv/CountUp.tsx
+++ src/components/countupdiv/CountUp.tsx
@@ -4,15 +4,26 @@ import IonIcon from "@reacticons/ionicons"
 //import up package
 import UpCount from "react-countup"
 
+
+let coutupInfor = [
+    {name: "Patient", logo: "newspaper", coutdata : 100}
+];
+
 const CountUp = () => {
   return (
     <div className="container mx-auto px-32">
         <div className="md:grid grid-cols-5 gap-2 md:my-12">
             <div className="border">
                 <div className="text-center">
-                    <p className="text-3xl"><IonIcon name="newspaper" size="large"></IonIcon></p>
+                    {/* <p className="text-3xl"><IonIcon name="newspaper" size="large"></IonIcon></p>
                     <p className="text-2xl">Patient</p>
-                    <p className="text-2xl"><UpCount end={100} /></p>
+                    <p className="text-2xl"><UpCount end={100} /></p> */}
+
+                    {
+                        coutupInfor.map((countinfo) => (
+                            <p className="">{countinfo.name}</p>
+                        ))
+                    }
                 </div>
             </div>
         </div>
