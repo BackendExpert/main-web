c59eef4d5d2211dea3aa4921e7d8c4593f1806e9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 12:49:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
 // import CountUp from "./components/countupdiv/CountUp";
 import Contact from "./components/contact/Contact";
-import Scientist from "./components/Scientist/Scientist";
+
 
 export default function App() {
   return (
@@ -14,7 +14,6 @@ export default function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/contact" element={<Contact />} ></Route>
-            <Route path="/Scientist" element={<Scientist />} ></Route>
           </Routes>
         </BrowserRouter>
       <Footer />
