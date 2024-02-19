7b7df10f252c3dc72b77283c94c8b762c6d09378
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-19 10:53:51 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/contact/Contact.tsx
+++ src/components/contact/Contact.tsx
@@ -1,7 +1,7 @@
 
 const Contact = () => {
   return (
-    <div className="container mx-auto px-32 py-8">
+    <div className="container mx-auto px-32 py-16">
         <h1 className="text-center font-semibold text-[#293352] text-3xl">Contact Us</h1>
     </div>
   )
