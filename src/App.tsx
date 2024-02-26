02d6e26d4162217440e896ac6558e8cf93efea98
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 21:37:21 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import NavBar from "./components/NavBar/NavBar";
 // import CountUp from "./components/countupdiv/CountUp";
 import Contact from "./components/contact/Contact";
 import Scientist from "./components/Scientist/Scientist";
-import TestArray from "./components/testArry/TestArray";
 import SciencPub from "./components/Science_Pub/SciencPub";
 import PubInfo from "./components/Science_Pub/PubInfo";
 
@@ -12,18 +11,27 @@ import PubInfo from "./components/Science_Pub/PubInfo";
 export default function App() {
   return (
     <div>
+      {/* include Nav bar */}
       <NavBar />
         {/* <CountUp /> */}
 
         <BrowserRouter>
           <Routes>
+            {/* Route for contact page */}
             <Route path="/contact" element={<Contact />} ></Route>
+
+            {/* Route for scientist page */}
             <Route path="/scientist" element={<Scientist />} ></Route>
-            <Route path="/testArry" element={<TestArray />} ></Route>
+
+            {/* Route for Science Publication page */}
             <Route path="/SciencePub" element={<SciencPub />} ></Route>
+
+            {/* Route for Science Publication more infor page */}
             <Route path="/SciencePub/PubInfo" element={<PubInfo />} ></Route>            
           </Routes>
         </BrowserRouter>
+
+        {/* include Footer */}
       <Footer />
     </div>
   )
