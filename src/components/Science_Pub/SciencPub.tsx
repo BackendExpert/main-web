2caebe44ed6560c9bd4f3fdf49cd384b20ad3c5b
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 20:24:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Science_Pub/SciencPub.tsx
+++ src/components/Science_Pub/SciencPub.tsx
@@ -15,15 +15,30 @@ const SciencPub = () => {
             title: "Awareness", 
             subdesc: "The Present Covid-19"
         },
+        {
+            img: "https://wallpapercave.com/wp/wp1870777.jpg", 
+            title: "Awareness", 
+            subdesc: "Stop the next pandemic? Stop illegal wildlife trade!"
+        },
+        {
+            img: "https://wallpapercave.com/wp/wp1870777.jpg", 
+            title: "Awareness", 
+            subdesc: "The Science of Aji-no-moto"
+        },
+        {
+            img: "https://wallpapercave.com/wp/wp1870777.jpg", 
+            title: "Awareness", 
+            subdesc: "The Present Covid-19"
+        },
     ]
   return (
     <div className="lg:px-32 py-24">
         <h1 className="text-center text-4xl font-semibold">Recent Scientific Publication</h1>
 
-        <div className="lg:flex justify-between my-16">
+        <div className="lg:grid grid-cols-3 gap-4 my-10">
             {
                 sciencePub.map((sciPub) => (
-                    <div className="mx-10">
+                    <div className="mx-8 my-8">
                         <img src={sciPub.img} alt="" className="w-full rounded-xl"/>
                         <p className="text-xl -mt-10 ml-4 text-white">{sciPub.title}</p>
                         <p className="mt-6 ml-2 text-xl">{sciPub.subdesc}</p>
