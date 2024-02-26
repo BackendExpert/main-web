4583e5fce29b04d56b4569893691300199e85922
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 19:51:50 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Science_Pub/SciencPub.tsx
+++ src/components/Science_Pub/SciencPub.tsx
@@ -1,24 +1,35 @@
 const SciencPub = () => {
     let sciencePub = [
         {
-            img: "", 
-            title: "", 
-            subdesc: ""
+            img: "https://wallpapercave.com/wp/wp1870777.jpg", 
+            title: "Awareness", 
+            subdesc: "Stop the next pandemic? Stop illegal wildlife trade!"
         },
         {
-            img: "", 
-            title: "", 
-            subdesc: ""
+            img: "https://wallpapercave.com/wp/wp1870777.jpg", 
+            title: "Awareness", 
+            subdesc: "The Science of Aji-no-moto"
         },
         {
-            img: "", 
-            title: "", 
-            subdesc: ""
+            img: "https://wallpapercave.com/wp/wp1870777.jpg", 
+            title: "Awareness", 
+            subdesc: "The Present Covid-19"
         },
     ]
   return (
     <div className="px-32 py-24">
         <h1 className="text-center text-4xl font-semibold">Recent Scientific Publication</h1>
+
+        <div className="">
+            {
+                sciencePub.map((sciPub) => (
+                    <div className={`bg-[url()]`}>
+                        
+                    </div>
+                ))
+            }
+        </div>
+
     </div>
   )
 }
