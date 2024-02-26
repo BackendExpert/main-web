96eba39aef0e1b75563fb856c3a7cceb0baffe26
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 21:13:05 +0530

    Updaet
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Science_Pub/PubInfo.tsx
+++ src/components/Science_Pub/PubInfo.tsx
@@ -5,6 +5,11 @@ const PubInfo = () => {
         <div className="text-center">
             <h1 className="text-4xl font-semibold">Stop the next pandemic? Stop illegal wildlife trade!</h1>
             <p className="pt-4 text-3xl">The role of Wildlife trade in transmitting emerging diseases</p>
+
+            <div className="lg:flex my-12">
+                <img src="https://wallpapercave.com/wp/wp1870777.jpg" alt="" className="lg:w-[50%] h-auto rounded-xl"/>
+                <p className="text-left lg:py-0 lg:pl-12 py-6 text-2xl">Hello World</p>
+            </div>
         </div>
     </div>
   )
