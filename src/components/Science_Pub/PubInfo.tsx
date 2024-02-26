98ffa0738a5bf1b3d7fa347cbf20cc7ff7971bd9
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 20:55:08 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Science_Pub/PubInfo.tsx
+++ src/components/Science_Pub/PubInfo.tsx
@@ -2,7 +2,7 @@
 const PubInfo = () => {
   return (
     <div className="lg:px-32 py-24">
-        <h1 className="text-center text-4xl font-semibold">Recent Scientific Publication</h1>
+        <h1 className="text-center text-4xl font-semibold">Stop the next pandemic? Stop illegal wildlife trade!</h1>
 
     </div>
   )
