73cb0fa04a75bc7c862e90c49c852370cc82be20
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 12:51:43 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/testArry/TestArray.tsx
+++ src/components/testArry/TestArray.tsx
@@ -1,6 +1,6 @@
 const TestArray = () => {
     let testArraies = [
-        {name: "kamal", email: "k@123", id: 1,},
+        {name: "kamal", email: "k@123", id: 1},
         {name: "Nimal", email: "n@123", id: 2},
         {name: "Amara", email: "a@123", id: 3},
         {name: "Saman", email: "s@123", id: 4},     
