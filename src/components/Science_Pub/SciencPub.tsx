81f0e00e956606bce6b0ae7b076d88ada1d042fa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 19:42:58 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Science_Pub/SciencPub.tsx
+++ src/components/Science_Pub/SciencPub.tsx
@@ -1,7 +1,7 @@
 const SciencPub = () => {
   return (
     <div className="px-32 py-24">
-        Recent Scientific Publication
+        <h1 className="text-center text-4xl font-semibold">Recent Scientific Publication</h1>
     </div>
   )
 }
