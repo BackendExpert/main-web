9e5dc0eb7cd5d2599008b9bb76fd5fb80e0bc8c1
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 11:39:46 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/testArry/TestArray.tsx
+++ src/components/testArry/TestArray.tsx
@@ -1,6 +1,15 @@
 const TestArray = () => {
+    let coutupInfor = [
+        {name: "kamal", email: "k@123", id: 1},
+        {name: "Nimal", email: "n@123", id: 2},
+        {name: "Amara", email: "a@123", id: 3},
+        {name: "Saman", email: "s@123", id: 4},
+     
+    ];
   return (
-    <div>TestArray</div>
+    <div className="px-32 py-20">
+        Test Arry
+    </div>
   )
 }
 
