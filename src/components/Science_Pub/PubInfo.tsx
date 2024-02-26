6d94ad86fcc9a2cabfd976370c38bb6c0ae5a2fa
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 21:21:53 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Science_Pub/PubInfo.tsx
+++ src/components/Science_Pub/PubInfo.tsx
@@ -5,13 +5,38 @@ const PubInfo = () => {
         <div className="text-center">
             <h1 className="text-4xl font-semibold">Stop the next pandemic? Stop illegal wildlife trade!</h1>
             <p className="pt-4 text-3xl">The role of Wildlife trade in transmitting emerging diseases</p>
-
+        </div>
             <div className="lg:flex my-12">
                 <img src="https://wallpapercave.com/wp/wp1870777.jpg" alt="" className="lg:w-[50%] h-auto rounded-xl"/>
                 <p className="text-left lg:py-0 lg:pl-12 py-6 text-2xl lg:mt-40">Dr. Shalini Rajakaruna <br />Published on Mon, 06/01/2020</p>
             </div>
+
+            <h1 className="text-2xl font-semibold py-4">Introduction</h1>
+            <div className="text-xl">
+                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui voluptate quasi itaque rem perferendis hic sapiente omnis saepe repellat laboriosam animi, nobis doloremque labore assumenda pariatur est autem, impedit voluptas?
+                Itaque, repellendus alias non ratione saepe amet aliquam repellat labore omnis numquam doloremque ea nemo quasi voluptas vero repudiandae voluptatibus at. Veniam, nulla porro. Incidunt laudantium doloremque adipisci totam ut!
+                Error temporibus, nam ex beatae, nemo optio ullam, quam nulla voluptatibus ipsam eos voluptatum tenetur et ab cum doloribus impedit. Reiciendis distinctio minus facere illum fugiat at odio minima deserunt?
+                Reprehenderit minima modi porro soluta quis. Nulla ex dignissimos, dolores laudantium enim adipisci a ducimus laborum eaque? Assumenda porro fuga iste dolore dolorem nobis voluptatum vel tempora magnam, impedit nulla?
+                In iste asperiores repudiandae tempora corporis. Commodi, qui, quidem soluta fuga maiores iusto quas obcaecati cumque ea saepe animi corporis exercitationem? Ullam quasi placeat a dolor, quos vero. Molestias, amet!
+                Placeat enim repellendus a blanditiis odit eum, explicabo sed voluptates cum voluptatum voluptatibus, magni possimus aspernatur eius error deleniti. Optio nulla fugiat soluta odit repellendus aliquam velit quis tempore vitae?
+                Atque, laudantium ipsum, commodi earum praesentium pariatur modi eveniet minus ea quia asperiores aspernatur molestias ullam voluptatem rem quae harum temporibus nisi fugit ipsam repellat repudiandae adipisci impedit minima! Voluptates.
+                At modi praesentium a dolore sequi et recusandae, deleniti totam iste sed, doloremque tempore animi harum! Modi, omnis, rerum explicabo labore ipsum eveniet minus animi, praesentium magni ratione doloremque odio!
+                Nihil, exercitationem sed? Aut tempore ratione saepe voluptates minus porro laboriosam accusamus ipsa veritatis, enim rem non maiores possimus quisquam ex voluptas! Quod, nostrum aliquid voluptatem in officiis praesentium sunt.
+                Ex labore tempore vitae possimus nam consequatur officiis soluta optio sapiente accusamus, dolor aperiam expedita quos reiciendis, libero vel suscipit cumque nostrum. Vero debitis alias architecto nostrum quisquam, omnis sit.
+            </div>
+            <h1 className="text-2xl font-semibold py-8">What is species spilover of pathogens?</h1>
+            <div className="text-xl">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita voluptates deserunt sequi dicta dolorum praesentium labore veniam nesciunt, maxime in officiis facilis quis suscipit provident incidunt, sapiente vel laborum? Odio.
+                Corporis tempore omnis obcaecati culpa ipsa assumenda facere libero ipsum, illo fugiat commodi ratione ex laudantium, mollitia tempora voluptatibus minus praesentium corrupti laboriosam consequuntur, expedita aliquid dolore. Fugiat, placeat similique.
+                Suscipit, quasi sapiente deserunt eligendi eveniet explicabo velit voluptates laudantium voluptatibus, dolorem corrupti id est? Eaque nemo quibusdam aspernatur aperiam modi expedita explicabo voluptates. Impedit necessitatibus asperiores doloremque sapiente quibusdam.
+                Corrupti pariatur perferendis aliquam accusamus vel? Dolorem magnam suscipit minus quisquam nihil repellendus sunt consequatur! Nesciunt enim, fuga quae laudantium quas repellendus sunt dolores vero voluptatem qui perferendis necessitatibus eum?
+                Laboriosam debitis, nostrum sit rem dolore suscipit non quaerat sapiente accusamus possimus praesentium ipsam, eos iste molestias cupiditate explicabo. Quasi dolorum, pariatur distinctio nobis fuga labore voluptas doloribus odio. Laudantium.
+            </div>
+            <div className="py-10 px-40">
+                <img src="https://wallpapercave.com/wp/wp1870777.jpg" alt="" className="lg:w-[100%] h-auto rounded-xl"/>
+            </div>
+            <h1 className="text-2xl font-semibold py-8">Controversial Origin of COVID-19; From Bats to Pangolins to Humans?</h1>
         </div>
-    </div>
   )
 }
 
