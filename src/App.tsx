93de9065e1bece80da2c9641541bfe88e2f9ca2d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 11:07:11 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import NavBar from "./components/NavBar/NavBar";
-// import CountUp from "./components/countupdiv/CountUp";
+import CountUp from "./components/countupdiv/CountUp";
 import Contact from "./components/contact/Contact";
 import Scientist from "./components/Scientist/Scientist";
 
@@ -10,14 +10,42 @@ export default function App() {
   return (
     <div>
       <NavBar />
-        {/* <CountUp /> */}
+        <div className="">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel, laudantium. Cum rerum doloribus soluta repudiandae quod nam aperiam magni aliquid culpa, exercitationem atque, sit quis totam delectus asperiores perspiciatis quia?
+          Doloribus, saepe dolore accusantium sit incidunt ipsam explicabo culpa impedit, in voluptatum sed nemo dolorum reprehenderit minima quod. Similique totam consequatur ex deserunt saepe necessitatibus repellat a, at fugiat quasi?
+          Ab esse inventore minus delectus distinctio! Minus enim aperiam perferendis similique odio ipsum, ut ratione pariatur libero quod, dignissimos corrupti necessitatibus. Obcaecati iusto earum amet accusantium dolor reiciendis deserunt deleniti?
+          Aliquid quibusdam praesentium aperiam maiores neque nesciunt cum architecto enim corrupti mollitia ex, eaque nobis non amet vitae reprehenderit dolores omnis numquam dolorem nisi, voluptatibus dolore magni! Distinctio, vitae nobis?
+          Harum porro eaque adipisci nesciunt repellendus sequi animi sed beatae ex voluptatibus expedita eveniet totam et laudantium ab, officia odio quasi quisquam veniam quam ad quas maxime. Placeat, dignissimos assumenda!
+          Impedit molestias, modi dolorum, laboriosam excepturi asperiores quos libero voluptatem autem consectetur nemo, reprehenderit blanditiis. Accusamus, beatae. Aut ducimus, dolorum amet fugiat, ab itaque eveniet ex ipsa cumque nam at.
+          Facilis porro deserunt doloribus vero distinctio tempora adipisci rerum quod dolore voluptate ducimus excepturi minus deleniti nobis tenetur culpa, autem eveniet ullam hic modi repellat. Reiciendis, nesciunt? Eveniet, consequatur autem.
+          Incidunt ducimus officiis vero neque earum nemo veritatis? Perspiciatis quasi, saepe obcaecati temporibus quidem facilis, suscipit possimus ad cumque esse, placeat aliquid accusantium. Soluta minima aperiam aliquid, maxime sed laboriosam!
+          Minima sapiente sed modi architecto nesciunt voluptate, tempora nisi, magni laborum, optio earum! Quibusdam natus, ipsam sapiente sunt molestias eius quis temporibus totam dicta dignissimos, provident omnis deserunt odio animi.
+          Officiis neque maxime dolore amet vel totam voluptate dolor! Alias, sit mollitia. Ullam, quam quidem aliquid, similique optio atque facilis voluptas qui maxime quibusdam accusamus, quisquam molestias quis suscipit. Esse.
+          Id fuga earum aut error, voluptatum ea magni porro tempora enim, asperiores doloribus perferendis esse voluptatem rem nostrum et ipsa possimus, quos dolore eveniet! Doloribus porro dolorum repellat! Neque, dolorem?
+          Vitae quasi sint repellat. Vero fugit eaque, explicabo labore praesentium et, neque officiis placeat similique sunt voluptatem aut vel dolores. Dicta vero hic maiores expedita consectetur animi neque aut voluptatum?
+          Sequi alias asperiores maxime tempora enim accusamus sit esse eius, minima quos autem sint eum suscipit ad aliquam earum? Ipsa deleniti, doloremque aperiam magnam fuga animi dolor quo quae. Similique.
+          Numquam commodi soluta nesciunt quod quidem cupiditate doloremque fugit error. Laborum autem maxime corporis illum dolorem explicabo eaque nam officiis iure voluptatibus! Sint eveniet rem quos reprehenderit numquam pariatur necessitatibus!
+          Commodi, aut. Dignissimos, modi cumque laudantium eum alias reiciendis laborum facilis delectus. Quod dignissimos molestiae architecto, reprehenderit aperiam commodi impedit at sunt? Explicabo quod alias eligendi iste nulla labore magni!
+        </div>
+        <CountUp />
 
-        <BrowserRouter>
+        <div className="">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maiores earum quae et rem deleniti voluptas. Aliquam provident vitae exercitationem sint eveniet praesentium dolores dignissimos tempore, alias, cumque deleniti fugit magnam?
+        Beatae minima, officia totam nisi quos illum doloribus officiis ducimus animi. Quibusdam, neque fugiat repellat ut numquam magnam. Tempore itaque tempora nesciunt? Nulla quas porro cum! Non, sapiente quibusdam! Maiores!
+        Assumenda cupiditate officiis a praesentium excepturi tempore repellat. Hic, ratione quam? Dicta explicabo, quisquam magni voluptate similique autem debitis consequatur at quibusdam, nihil ut eaque et optio sapiente neque recusandae!
+        Iure voluptatum voluptatem accusamus libero, incidunt eveniet. Quibusdam, ullam commodi officiis praesentium corporis qui harum velit totam obcaecati ratione neque repellat voluptates. Odit numquam quis ipsam laboriosam ratione quos dolore?
+        Nulla id non atque accusamus voluptatibus nam asperiores pariatur, aliquam officiis iure cumque veniam sunt odit nihil aut architecto molestiae dolorum repellat laboriosam rem facere ipsam dicta quasi quidem. Id!
+        Veritatis exercitationem soluta sunt impedit nobis quaerat dicta assumenda labore. Dignissimos iure voluptatibus dolorum porro ratione nobis molestias animi earum veniam, magnam eligendi maiores, eum quasi? Debitis fuga quibusdam officiis.
+        Maxime nihil, cumque reiciendis impedit asperiores, excepturi, nesciunt dolor eos cum modi rem iure quaerat a dolorem quibusdam sed natus alias dignissimos voluptates aspernatur dicta quae? Repellendus similique magni provident.
+        Et doloribus alias similique aut sed omnis, optio maiores officia magni expedita ad iusto quibusdam qui, amet soluta dicta? Doloribus sequi officia voluptate repudiandae asperiores deleniti natus sapiente cupiditate unde.
+        Facere tenetur ipsa enim dolore dolor. Deleniti sit obcaecati deserunt, qui culpa nobis voluptate odit debitis laudantium nihil, maiores autem ab maxime rem non suscipit eum placeat voluptates. Repudiandae, omnis.
+        Amet maxime autem, voluptates, quis fugit in et, eos magnam dignissimos eveniet ipsa reiciendis totam alias non neque! Quam, sunt. Placeat sapiente repellat ratione quibusdam ex tempora ab ad? Aspernatur.</div>
+
+        {/* <BrowserRouter>
           <Routes>
             <Route path="/contact" element={<Contact />} ></Route>
             <Route path="/scientist" element={<Scientist />} ></Route>
           </Routes>
-        </BrowserRouter>
+        </BrowserRouter> */}
       <Footer />
     </div>
   )
