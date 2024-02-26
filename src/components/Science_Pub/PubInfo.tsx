dda29f985cb10f039c7dbd04d0b33b3f864f5510
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 21:00:34 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Science_Pub/PubInfo.tsx
+++ src/components/Science_Pub/PubInfo.tsx
@@ -2,8 +2,10 @@
 const PubInfo = () => {
   return (
     <div className="lg:px-32 py-24">
-        <h1 className="text-center text-4xl font-semibold">Stop the next pandemic? Stop illegal wildlife trade!</h1>
-
+        <div className="text-center">
+            <h1 className="text-4xl font-semibold">Stop the next pandemic? Stop illegal wildlife trade!</h1>
+            <p className="pt-4 text-3xl">The role of Wildlife trade in transmitting emerging diseases</p>
+        </div>
     </div>
   )
 }
