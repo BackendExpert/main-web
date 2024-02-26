244529330be6b26ca2f1f5e34b6392e6e5f25285
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 13:14:36 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/testArry/TestArray.tsx
+++ src/components/testArry/TestArray.tsx
@@ -1,9 +1,9 @@
 const TestArray = () => {
     let testArraies = [
+        {name: "Jehan", email: "k@123" },
         {name: "kamal", email: "k@123" },
         {name: "Nimal", email: "n@123" },
         {name: "Amara", email: "a@123" },
-        {name: "Saman", email: "s@123" },     
     ];
 
 
@@ -13,7 +13,7 @@ const TestArray = () => {
         {
             testArraies.map((testArray, index) => {
                 if(index == 0){
-                    return <p className="">{testArray.name}</p>
+                    return <p className="text-red-500 text-3xl">{testArray.name}</p>
                 }
                 else{
                     return <p className="">{testArray.name}</p>
