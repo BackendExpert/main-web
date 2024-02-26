193ee91f0f876b1a745792926d8da3a1cf276392
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 19:45:33 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Science_Pub/SciencPub.tsx
+++ src/components/Science_Pub/SciencPub.tsx
@@ -1,4 +1,21 @@
 const SciencPub = () => {
+    let sciencePub = [
+        {
+            img: "", 
+            title: "", 
+            subdesc: ""
+        },
+        {
+            img: "", 
+            title: "", 
+            subdesc: ""
+        },
+        {
+            img: "", 
+            title: "", 
+            subdesc: ""
+        },
+    ]
   return (
     <div className="px-32 py-24">
         <h1 className="text-center text-4xl font-semibold">Recent Scientific Publication</h1>
