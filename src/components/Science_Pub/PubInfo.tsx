9ce596da7f6f303582f8e685f80f15e1685e77b2
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 21:24:57 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/Science_Pub/PubInfo.tsx
+++ src/components/Science_Pub/PubInfo.tsx
@@ -36,6 +36,49 @@ const PubInfo = () => {
                 <img src="https://wallpapercave.com/wp/wp1870777.jpg" alt="" className="lg:w-[100%] h-auto rounded-xl"/>
             </div>
             <h1 className="text-2xl font-semibold py-8">Controversial Origin of COVID-19; From Bats to Pangolins to Humans?</h1>
+            <div className="text-xl py-8">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis commodi laboriosam illum exercitationem officiis sunt perspiciatis dolore, inventore odit corporis quasi, incidunt fuga, non repellat ratione sit iure. Iure, dolore?
+                Eum nisi dolores exercitationem, minus ipsam ea vel animi illum cumque magnam suscipit ex quaerat velit omnis quas blanditiis numquam hic veniam tempora recusandae fuga magni? Dolores eveniet tenetur magni.
+                Ut, cumque. Sint harum et ipsam nihil commodi voluptas at, ratione aliquid? Mollitia, minus? Veritatis et praesentium excepturi distinctio a, veniam, ut blanditiis, illum quis vel nemo voluptates ratione expedita!
+                Eum ratione delectus quasi a obcaecati ea voluptate? Nam veritatis distinctio blanditiis numquam labore accusantium culpa. Dolores neque, reprehenderit excepturi mollitia maiores voluptatibus qui, aut consequuntur laboriosam, assumenda veniam aliquam?
+                Autem consequuntur porro, quisquam doloribus rem vitae repellendus maxime accusamus? Harum possimus quibusdam, velit eaque cum quas a, rerum ipsam eos odit quos incidunt dolor. Delectus culpa cupiditate sapiente corrupti?
+                Iusto a consectetur inventore! Excepturi quos tenetur praesentium ipsum ipsam adipisci sapiente impedit possimus atque iste sequi, veniam harum saepe veritatis iusto facere expedita? Architecto voluptate sunt quo eveniet modi?
+                Labore laboriosam itaque architecto distinctio voluptatum quia cumque consectetur error fuga, maxime minus enim earum velit perferendis sequi nesciunt impedit autem blanditiis vero omnis dignissimos perspiciatis molestiae. Deleniti, magni non!
+                Aut aperiam quisquam sapiente, nesciunt similique, illo, ratione placeat blanditiis optio aspernatur eaque non necessitatibus voluptatem nostrum atque? Vel rerum provident iusto dolor quaerat distinctio harum exercitationem consectetur temporibus eligendi.
+            </div>
+            <div className="text-xl py-8">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente, eius. Consequuntur eos rem blanditiis molestiae soluta quibusdam? Voluptatibus, voluptates corporis. Cumque, ratione repellat eum nobis sit odio earum tenetur perferendis.
+                Ex veniam a, sed harum tempora enim, necessitatibus voluptatibus quia, mollitia maiores sit deserunt numquam neque illum blanditiis excepturi deleniti? Magnam odit, eveniet sapiente nulla ea explicabo ad repudiandae. Doloremque.
+                Architecto fugiat impedit aperiam expedita sequi laborum! Eveniet, nobis in? Optio explicabo natus tempora nostrum officia esse eaque culpa ex aperiam aspernatur, eius doloribus enim saepe, libero molestias, soluta at!
+                Ipsum debitis repellat nam voluptatum ea veniam error. Adipisci est ipsa eveniet voluptatum a expedita, cumque quos nostrum quis nam, optio doloribus quam, eum ex iusto ullam harum corrupti cum?
+            </div>
+            <div className="text-xl py-8">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam eaque commodi incidunt natus laudantium omnis fugiat ut obcaecati, voluptatibus dicta iste. Assumenda voluptatem voluptate quos impedit est distinctio nobis ut.
+                Veritatis mollitia dolore doloremque reprehenderit dicta natus praesentium inventore ea eveniet repellat cumque expedita ratione quidem tempora earum beatae voluptatem magnam, aliquam deleniti nemo minus consequatur odio. Voluptates, doloribus necessitatibus.
+                Quo optio porro quas illo deserunt eveniet! A architecto vitae nesciunt deleniti. Obcaecati sequi voluptatibus quo, qui, aut exercitationem, eos autem assumenda reprehenderit vero natus? Ducimus cupiditate facere autem quidem.
+                Ab amet explicabo ad. Consequuntur obcaecati, deleniti provident veritatis velit rerum ipsam hic modi dolores adipisci recusandae sit illo, maxime nam accusantium nemo architecto voluptas magnam quo quia laborum? Veritatis.
+                Tempore maiores placeat doloremque iusto eum molestias tempora consequuntur voluptates dolor maxime quidem explicabo officia laborum quaerat ipsum, nihil esse tenetur, quisquam itaque. Distinctio molestiae veniam, cupiditate eos similique quam?
+                Consequuntur et provident, impedit tempore sint sapiente ratione fugit voluptates soluta quo repudiandae quasi possimus fugiat maxime nihil commodi cum ipsam voluptas architecto sequi laudantium ad. Qui recusandae minima voluptatibus.
+                Vitae blanditiis vero veritatis adipisci consequatur quam veniam ratione aliquid laborum omnis temporibus autem aut odio iusto nisi possimus pariatur debitis non, fugiat eveniet illum quod magni odit. Ab, accusantium?
+                Illum exercitationem sapiente voluptatem asperiores iusto. Aut officiis autem debitis error quaerat vel non provident nisi mollitia dolor nemo quas culpa magni, blanditiis consequuntur cupiditate excepturi necessitatibus qui ex voluptates.
+                Provident sint voluptatem atque et odit molestias quaerat ab, consequuntur illum saepe rem similique quae enim? Totam cum quam temporibus perspiciatis placeat animi quisquam numquam adipisci, consequatur, repudiandae dolores voluptatem.
+                Culpa nam autem officia ratione modi laboriosam vero quibusdam, rem eum quasi error est deleniti dicta ipsam. Molestiae doloremque ratione repellat asperiores alias optio nemo laborum? Aspernatur laborum alias hic.
+                Voluptatibus reiciendis deleniti cumque? Eligendi at obcaecati exercitationem quae nisi porro reiciendis veritatis alias cumque cupiditate mollitia vero iusto, optio enim deserunt aperiam amet, ex eos! Excepturi exercitationem alias tempora.
+                Eum sunt atque debitis quos provident maxime placeat voluptates beatae tenetur, illo, qui voluptatum optio dolor reiciendis magni perferendis sequi earum esse! Ipsa nobis modi at quod. Odio, voluptatem itaque.
+            </div>
+            <div className="py-10 px-40">
+                <img src="https://wallpapercave.com/wp/wp1870777.jpg" alt="" className="lg:w-[100%] h-auto rounded-xl"/>
+            </div>
+            <h1 className="text-2xl font-semibold py-8">Other Pandemics of this century and wildlife trade</h1>
+            <div className="text-xl py-8">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate similique facilis vero a voluptates repellendus dolores libero impedit labore ipsum et doloribus non atque eum suscipit, aspernatur omnis, vitae optio?
+                Adipisci quibusdam consectetur, repellat neque voluptatum earum distinctio, iste pariatur quam ducimus officiis quaerat quo doloremque incidunt vel vitae obcaecati. Voluptates officia pariatur corporis doloremque aliquid provident eum nemo molestiae.
+                Nihil cumque culpa vel veniam facilis voluptatum labore, voluptatem distinctio mollitia ipsa consequuntur dolorem itaque dolore accusantium numquam sint at ab voluptate ducimus dignissimos! Veritatis, unde facilis. Eos, ea minima?
+                Dignissimos dolor saepe sint laborum possimus asperiores eveniet ratione unde autem laudantium hic exercitationem quasi enim in, debitis quo ipsam! Rem quo necessitatibus labore at ducimus? Consequatur possimus obcaecati rerum.
+                Placeat, molestiae aliquid similique laborum at porro dolores, praesentium, cupiditate est sapiente nam reprehenderit amet assumenda quasi odit neque iste dolor nemo. Sit saepe sint cumque velit reiciendis dolorem doloribus?
+                Ea ducimus reprehenderit ullam fugiat necessitatibus id fugit obcaecati, quia nostrum minus mollitia. In exercitationem explicabo voluptas cupiditate expedita. Nisi veritatis fugit sint harum impedit iure tempore repellendus ullam quibusdam?
+                Perferendis dolorem possimus porro eos pariatur sunt eveniet dicta, magni vitae sapiente cum iste doloremque iusto voluptatibus libero asperiores consequatur rerum blanditiis doloribus delectus consequuntur rem quas. Doloribus, ut odit.
+            </div>
         </div>
   )
 }
