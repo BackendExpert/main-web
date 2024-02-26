308de3710c3e9cf2e6151850c0e7c8fa9689486d
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-26 12:51:06 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/testArry/TestArray.tsx
+++ src/components/testArry/TestArray.tsx
@@ -1,20 +1,26 @@
 const TestArray = () => {
     let testArraies = [
-        {name: "kamal", email: "k@123", id: 1},
+        {name: "kamal", email: "k@123", id: 1,},
         {name: "Nimal", email: "n@123", id: 2},
         {name: "Amara", email: "a@123", id: 3},
-        {name: "Saman", email: "s@123", id: 4},
-     
+        {name: "Saman", email: "s@123", id: 4},     
     ];
+
+
+
   return (
     <div className="px-32 py-20">
         {
-            testArraies.map((textArray) => (
-                <div className="">
-                    <p className="font-bold text-3xl">{textArray.name}</p>
-                    <p className="">{textArray.email}</p>
-                </div>
-            ))
+            testArraies.map((testArray) => {
+                if(testArray.id == 1){
+                    return (
+                        <div className="text-red-500">{testArray.name}</div>
+                    );
+                }
+                return (
+                    <div className="text-blue-500">{testArray.name}</div>
+                );
+            })
         }
     </div>
   )
