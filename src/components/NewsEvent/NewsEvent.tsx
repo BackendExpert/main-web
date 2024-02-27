5102c279147088d71d97becbb542184091d37174
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-27 14:28:00 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/NewsEvent/NewsEvent.tsx
+++ src/components/NewsEvent/NewsEvent.tsx
@@ -6,14 +6,15 @@ const NewsEvent = () => {
   return (
     <div className="lg:px-32 px-8 py-24">
         <h1 className="text-3xl font-semibold text-center">News and Events</h1>
-        
-        {
+        <div className="">
+          {
             newsevetlist.map((news) => (
-                <div className="">
-                    <p className="">{news.title}</p>
-                </div>
+              <div className="">
+                <img src={news.img} alt="" />
+              </div>
             ))
-        }
+          }
+        </div>        
     </div>
   )
 }
--- src/components/NewsEvent/NewsEventList.tsx
+++ src/components/NewsEvent/NewsEventList.tsx
@@ -8,19 +8,19 @@ import MainH from "../../assets/mainh.jpg"
 const newsevetlist = [
     {
         link: "#",
-        img: {Profs},
+        img: Profs,
         title: "Award and Recognition",
         subdesc: "Three NIFS Researchers ranked Among Top 2% Scientists in the World - Elsevier/Stanford Citation Analysis"
     },
     {
         link: "#",
-        img: {School},
+        img: School,
         title: "Compitition",
         subdesc: "Champions of the Research Competition"
     },    
     {
         link: "#",
-        img: {MainH},
+        img: MainH,
         title: "Press Release",
         subdesc: "The NIFS inputs have been fruitful"
     },
@@ -29,19 +29,19 @@ const newsevetlist = [
 
     {
         link: "#",
-        img: {Profs},
+        img: Profs,
         title: "Special Lectures",
         subdesc: "Advanced Raman Spectroscopy of Carbon Nanomaterials"
     },
     {
         link: "#",
-        img: {Win},  
+        img: Win,  
         title: "Award and Recognition",
         subdesc: "NIFS Volunteer Researcher Honored with Gold Medal"
     },
     {
         link: "#",
-        img: {MainH},
+        img: MainH,
         title: "Conference",
         subdesc: "NIFS 2024 Conference on Novermber 2024"
     },
