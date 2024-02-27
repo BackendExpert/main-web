f5189c0c8de953226966c69bf6d2bd7e23083ddd
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-27 06:51:14 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/NewsEvent/NewsEvent.tsx
+++ src/components/NewsEvent/NewsEvent.tsx
@@ -1,9 +1,16 @@
 
 const NewsEvent = () => {
-    
+
   return (
     <div className="lg:px-32 px-8 py-24">
         <h1 className="text-3xl font-semibold text-center">News and Events</h1>
+        {
+            newsevetlist.map((news) => (
+                <div className="">
+                    <p className="">{news.link}</p>
+                </div>
+            ))
+        }
     </div>
   )
 }
