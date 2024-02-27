a489aac005d5634236309510180767d44beb92ed
JehanKandy <138636749+BackendExpert@users.noreply.github.com>
2024-02-27 06:40:52 +0530

    Update
    Co-Authored-By: JehanKandy  <jehankandy@gmail.com>

--- src/components/NewsEvent/NewsEvent.tsx
+++ src/components/NewsEvent/NewsEvent.tsx
@@ -1,7 +1,10 @@
 
 const NewsEvent = () => {
+    
   return (
-    <div className="lg:px-32 px-8 py-24">NewsEvent</div>
+    <div className="lg:px-32 px-8 py-24">
+        <h1 className="text-3xl font-semibold text-center">News and Events</h1>
+    </div>
   )
 }
 
